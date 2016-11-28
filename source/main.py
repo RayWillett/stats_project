2f18a4c08563cf93524925d097677a5f1dcd5ea8
ray <ray@localhost.localdomain>
2016-11-28 13:51:29 -0500

    trendlines

--- source/main.py
+++ source/main.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 import matplotlib.pyplot as plt
 
 #DEBUG MODES
@@ -26,11 +27,30 @@ def get_data_and_noise(s, e): #start, end, noise's range
         list.append(data)
     return list
 
+def get_trendline(xs, ys, degree):
+    list = []
+    z = np.polyfit(xs, ys, degree)
+    for i, x in enumerate(xs):
+        yhat = -1
+        if degree == 1:
+            yhat = x*z[0] + z[1]
+        elif degree == 2:
+            yhat = (x**2)*z[0] + x*z[1] + z[2]
+        elif degree == 3:
+            yhat = (x**3)*z[0] + (x**2)*z[1] + x*z[2] + z[3]
+        elif degree == 4:
+            yhat = (x**4)*z[0] + (x**3)*z[1] + (x**2)*z[2] + x*z[3] + z[4]
+        else:
+            raise Exception("INVALID DEGREE")
+        list.append(yhat)
+    return list
+
 list = get_data_and_noise(0, 500)
 
 xs = [x for (x,y,o) in list] #list of x values
 ys = [y for (x,y,o) in list] #list of noisy data values
 os = [o for (x,y,o) in list] #list of true function values
+ls = get_trendline(xs, ys, 1)
 
 plt.scatter(xs, ys)
 
@@ -38,7 +58,8 @@ if show_true:
     plt.plot(xs, os, color='r') #graph true line NOT the trendline
 
 
-
+#show linear trendline
+#plt.plot(xs, ls, color='g')
 
 plt.show()
 if debug:
